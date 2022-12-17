8028f5670a7a7b4e29197bab021b6449a3044be9
Konstantin D <84802550+niceguy135@users.noreply.github.com>
2022-12-17 19:38:31 +0300

    Update rangefinder_hcsr04.c
    Исправленный дальномер

--- stage7/rangefinder_hcsr04.c
+++ stage7/rangefinder_hcsr04.c
@@ -15,7 +15,7 @@
 #define LOW 0
 #define HIGH 1
 
-#define VAR7_1
+#define VAR7_2 1
 
 #ifdef VAR2
 int TRIG = 8; // GPIO PIN TRIG
@@ -117,37 +117,16 @@ static int GPIODirection(int pin, int dir) {
 	return (0);
 }
 
-static int GPIORead(int pin) {
 #define VALUE_MAX 30
-	char path[VALUE_MAX];
-	char value_str[3];
-	int fd;
-
-	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
-	fd = open(path, O_RDONLY);
-	if (-1 == fd) {
-		fprintf(stderr, "Failed to open gpio value for reading!\n");
-		Exiting(-1);
-	}
-
-	if (-1 == read(fd, value_str, 3)) {
-		fprintf(stderr, "Failed to read value!\n");
-		Exiting(-1);
-	}
-
-	close(fd);
-
-	return (atoi(value_str));
-}
 
 static int driverWork(){
 	char value_str[3];
 	int fd;
-	char *path = "/sys/module/<имя_модуля>/parameters/echo_value"; // че бля за имя модуля
+	char *path = "/sys/module/driver/parameters/echo_value"; // че бля за имя модуля
 
 	fd = open(path, O_RDONLY);
 	if (-1 == fd) {
-		fprintf(stderr, "Failed to open gpio value for reading!\n");
+		fprintf(stderr, "Failed to open gpio value for reading!");
 		Exiting(-1);
 	}
 
@@ -240,7 +219,7 @@ void sigfunc(int sig) {
 
 int main(int argc, char *argv[]) {
 
-	signal(SIGINT, sigfunc);
+	//signal(SIGINT, sigfunc);
 
 	int quiet = 0;
 
@@ -313,9 +292,9 @@ int main(int argc, char *argv[]) {
 		char range_str[32] = {0};
 		sprintf(range_str, "%0.6f\n", search_time);
 
-		if (writeFifo(named_pipe, range_str) < 0) {
-			return -1;
-		}
+		// if (writeFifo(named_pipe, range_str) < 0) {
+		// 	return -1;
+		// }
 
 		if (!quiet)
 			printf("signal_delay: %lf\nTime = %s", search_time, asctime(timeinfo));
